e0f888b6e7ff12a0ab091649f3969565a82d2976
peterzen <peter@froggle.org>
2025-06-17 10:30:09 +0200

    refactor: use dynamic doodle icons

---
+++ apps/frontend/src/components/icons/DoodleIcons.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import { doodleIcons, type DoodleIconName } from '@/icons/doodleIcons'
+
+  const props = defineProps<{
+    name: DoodleIconName
+    class?: string
+  }>()
+
+  const Component = doodleIcons[props.name]
+</script>
+
+<template>
+  <component :is="Component" v-if="Component" :class="props.class" />
+</template>
---
+++ apps/frontend/src/icons/doodleIcons.ts
@@ -0,0 +1,45 @@
+import IconSun from '@/assets/icons/interface/sun.svg'
+import IconMail from '@/assets/icons/interface/mail.svg'
+import IconMessage from '@/assets/icons/interface/message.svg'
+import IconPhone from '@/assets/icons/interface/phone.svg'
+import IconTick from '@/assets/icons/interface/tick.svg'
+import IconLogin from '@/assets/icons/interface/login.svg'
+import IconArrowSingleLeft from '@/assets/icons/arrows/arrow-single-left.svg'
+import IconMenuDotsVert from '@/assets/icons/interface/menu-dots-vert.svg'
+import IconPen from '@/assets/icons/interface/pen.svg'
+import IconSearch from '@/assets/icons/interface/search.svg'
+import IconSetting2 from '@/assets/icons/interface/setting-2.svg'
+import IconUser from '@/assets/icons/interface/user.svg'
+import IconLogout from '@/assets/icons/interface/logout.svg'
+import IconGlobe from '@/assets/icons/interface/globe.svg'
+import IconMic from '@/assets/icons/interface/mic.svg'
+import IconMic2 from '@/assets/icons/interface/mic-2.svg'
+import IconArrowLeft from '@/assets/icons/arrows/arrow-left.svg'
+import IconCamera2 from '@/assets/icons/interface/camera.svg'
+import IconPhoto from '@/assets/icons/interface/photo.svg'
+import IconCross from '@/assets/icons/interface/cross.svg'
+
+export const doodleIcons = {
+  IconSun,
+  IconMail,
+  IconMessage,
+  IconPhone,
+  IconTick,
+  IconLogin,
+  IconArrowSingleLeft,
+  IconMenuDotsVert,
+  IconPen,
+  IconSearch,
+  IconSetting2,
+  IconUser,
+  IconLogout,
+  IconGlobe,
+  IconMic,
+  IconMic2,
+  IconArrowLeft,
+  IconCamera2,
+  IconPhoto,
+  IconCross,
+}
+
+export type DoodleIconName = keyof typeof doodleIcons
