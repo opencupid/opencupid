025cdecf2fa8f99a75179f7aff4d2e8ad0d6a137
peterzen <peter@froggle.org>
2025-07-01 10:44:00 +0200

    Add Matches view and route

---
+++ apps/frontend/src/features/interaction/views/Matches.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import MatchesList from '../components/MatchesList.vue'
+import { useDatingInteractions } from '../composables/useDatingInteractions'
+
+const { matches, refreshInteractions } = useDatingInteractions()
+
+onMounted(() => {
+  refreshInteractions()
+})
+</script>
+
+<template>
+  <main class="container mt-3 pt-5">
+    <MatchesList :edges="matches" />
+  </main>
+</template>
