be679aa02fdb5b5e25f6f673d9482aa06d0cb9c3
Peter Banik <peter@froggle.org>
2025-07-01 19:16:09 +0200

    Messaging fixes

--- apps/frontend/src/features/messaging/views/Messaging.vue
+++ apps/frontend/src/features/messaging/views/Messaging.vue
@@ -5,11 +5,14 @@ import { computed, onMounted, onUnmounted, ref, watch } from 'vue'
 import type { ConversationSummary } from '@zod/messaging/messaging.dto'
 import type { ProfileSummary } from '@zod/profile/profile.dto'
 
+import MiddleColumn from '@/features/shared/ui/MiddleColumn.vue'
+import IconMessage from '@/assets/icons/interface/message.svg'
+import IconSearch from '@/assets/icons/interface/search.svg'
+
 import ConversationDetail from '../components/ConversationDetail.vue'
 import { useMessageStore } from '../stores/messageStore'
 import ConversationSummaries from '../components/ConversationSummaries.vue'
-import IconMessage from '@/assets/icons/interface/message.svg'
-import IconSearch from '@/assets/icons/interface/search.svg'
+import ViewTitle from '../../shared/ui/ViewTitle.vue'
 
 const router = useRouter()
 const messageStore = useMessageStore()
@@ -59,18 +62,18 @@ const handleSelectConvo = async (convo: ConversationSummary) => {
 
 const handleDeselectConvo = async () => {
   router.back()
-  await messageStore.setActiveConversation(null)
+  messageStore.resetActiveConversation()
 }
 
 const handleProfileSelect = (profile: ProfileSummary) => {
-  router.push({ name: 'PublicProfile', params: { id: profile.id } })
+  router.push({ name: 'PublicProfile', params: { profileId: profile.id } })
 }
 
 const haveConversations = computed(() => {
   return messageStore.conversations.length > 0
 })
 
-const isDetailView = computed(() => !!messageStore.activeConversation && !messageStore.isLoading)
+const isDetailView = computed(() => !!messageStore.activeConversation)
 </script>
 
 <template>
@@ -78,41 +81,41 @@ const isDetailView = computed(() => !!messageStore.activeConversation && !messag
     <!-- Detail view overlay -->
     <div
       v-if="isDetailView"
-      class="position-absolute top-0 start-0 w-100 h-100"
-      style="z-index: 1050;"
+      class="detail-view position-absolute w-100"
+      style="z-index: 1050"
     >
-      <div class="overflow-auto h-100">
+      <MiddleColumn class="h-100">
         <ConversationDetail
           :loading="messageStore.isLoading"
           :conversation="messageStore.activeConversation"
           @deselect:convo="handleDeselectConvo"
           @profile:select="handleProfileSelect"
+          @updated="messageStore.fetchConversations"
         />
-      </div>
+      </MiddleColumn>
     </div>
 
     <!-- List view -->
     <div class="d-flex flex-column h-100" :class="{ 'd-none': isDetailView }">
+      <ViewTitle :icon="IconMessage" title="Messages" class="text-primary" />
       <BOverlay
         :show="!haveConversations"
         no-spinner
-        bg-color="var(--bs-body-bg)"
+        bg-color="inherit"
         :blur="null"
         opacity="0.85"
         class="h-100 overlay"
       >
         <template #overlay>
           <div class="d-flex flex-column align-items-center justify-content-center h-100">
-            <IconMessage class="svg-icon-100 opacity-25" />
-            <p class="text-muted mb-4 mt-4 text-center">
-              Your conversations will take place here.
-            </p>
+            <p class="text-muted mb-4 mt-4 text-center">Your conversations will take place here.</p>
             <BButton
               variant="primary"
               size="lg"
               pill
               @click="router.push({ name: 'BrowseProfiles' })"
             >
+              <IconSearch class="svg-icon" />
               Find people to talk to
             </BButton>
           </div>
@@ -120,17 +123,42 @@ const isDetailView = computed(() => !!messageStore.activeConversation && !messag
 
         <!-- Conversation summaries -->
         <div class="flex-grow-1 overflow-auto pt-5">
-          <div class="mx-3">
+          <MiddleColumn>
             <ConversationSummaries
               :loading="messageStore.isLoading"
               :conversations="messageStore.conversations"
               :activeConversation="messageStore.activeConversation"
               @convo:select="handleSelectConvo"
             />
-          </div>
+          </MiddleColumn>
         </div>
       </BOverlay>
     </div>
   </main>
 </template>
-<style scoped></style>
+<style scoped lang="scss">
+@import 'bootstrap/scss/functions';
+@import 'bootstrap/scss/variables';
+@import 'bootstrap/scss/mixins';
+@import '@/css/app-vars.scss';
+
+.detail-view {
+  left: 0;
+  // nav.fixed is on 1030 - on screens < md we put this above the navbar
+
+  // on screens > sm navbar stays visible
+  top: $navbar-height;
+  height: calc(100vh - $navbar-height);
+  z-index: 900;
+}
+
+.inactive {
+  pointer-events: none;
+  visibility: hidden;
+  display: none;
+}
+main {
+  width: 100%;
+  // height: 100vh;
+}
+</style>
