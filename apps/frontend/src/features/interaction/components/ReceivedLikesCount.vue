b7ad109116e5a93fd137a6a811a6f9b05e79358f
Peter Banik <peter@froggle.org>
2025-07-02 12:57:42 +0200

    Matches page finetuning

--- apps/frontend/src/features/interaction/components/ReceivedLikesCount.vue
+++ apps/frontend/src/features/interaction/components/ReceivedLikesCount.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
 import IconHeart from '@/assets/icons/interface/heart.svg'
-import { useInteractionsViewModel } from '../composables/useInteractionsViewModel'
 
-const { receivedLikesCount, refreshInteractions, isLoading } = useInteractionsViewModel()
-
-onMounted(() => {
-  refreshInteractions()
-})
+defineProps<{
+  receivedLikesCount: number
+}>()
 </script>
 
 <template>
