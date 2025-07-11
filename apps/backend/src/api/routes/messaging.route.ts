d55c0c8ab4e3c1987544b57601995d5c4e70a5bd
Peter Banik <peter@froggle.org>
2025-07-11 18:05:25 +0200

    Send notification only when user is not logged on

--- apps/backend/src/api/routes/messaging.route.ts
+++ apps/backend/src/api/routes/messaging.route.ts
@@ -158,19 +158,19 @@ const messageRoutes: FastifyPluginAsync = async fastify => {
       reply.code(200).send(response)
 
       // Broadcast the new message to the recipient's WebSocket connections
-      const ok = broadcastToProfile(fastify, profileId, {
+      const isWsBroadcasted = broadcastToProfile(fastify, profileId, {
         type: 'ws:new_message',
         payload: messageDTO,
       })
 
-      webPushService.send(messageDTO)
-
-      await notifierService.notifyProfile(profileId, 'new_message', {
-        sender: messageDTO.sender.publicName,
-        message: messageDTO.content,
-        link: `${appConfig.FRONTEND_URL}/messages`,
-      })
-
+      if (!isWsBroadcasted) {
+        await notifierService.notifyProfile(profileId, 'new_message', {
+          sender: messageDTO.sender.publicName,
+          message: messageDTO.content,
+          link: `${appConfig.FRONTEND_URL}/inbox`,
+        })
+      // webPushService.send(messageDTO)
+      }
     } catch (error: any) {
       return sendError(reply, 403, error)
     }
