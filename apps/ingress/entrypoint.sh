5eeece97a67e7d2fff3835ab833770fdf0792b0f
Peter Banik <peter@froggle.org>
2026-03-03 01:50:24 +0100

    ingress certbot refactor

---
+++ apps/ingress/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -eu
+
+CONF=/usr/local/openresty/nginx/conf
+
+envsubst '${DOMAIN} ${ADMIN_DOMAIN}' \
+  < "$CONF/conf.d/https.conf.tmpl" \
+  > "$CONF/conf.d/https.conf"
+
+CERT="/etc/letsencrypt/live/${DOMAIN}/fullchain.pem"
+KEY="/etc/letsencrypt/live/${DOMAIN}/privkey.pem"
+
+if [ -f "$CERT" ] && [ -f "$KEY" ]; then
+  ln -sf "$CONF/conf.d/https.conf" "$CONF/conf.d/active.conf"
+else
+  ln -sf "$CONF/conf.d/http-bootstrap.conf" "$CONF/conf.d/active.conf"
+fi
+
+exec openresty -g "daemon off;"
