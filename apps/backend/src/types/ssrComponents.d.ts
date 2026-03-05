7f4bee8fafe2d906a138247b481b5c3af352a34b
Peter Banik <peter@froggle.org>
2026-03-05 16:14:20 +0100

    fix lint errors

--- apps/backend/src/types/ssrComponents.d.ts
+++ apps/backend/src/types/ssrComponents.d.ts
@@ -1,7 +1,7 @@
-declare module "../../../dist-ssr/EmailTemplate.ssr.mjs" {
-  type DefineComponent = import("vue").DefineComponent;
-  type SimpleEmailProps = import("../services/email/types").EmailTemplateProps;
+declare module '*.ssr.mjs' {
+  type DefineComponent = import('vue').DefineComponent
+  type EmailTemplateProps = import('../services/email/types').EmailTemplateProps
 
-  const component: DefineComponent<SimpleEmailProps, {}, any>;
-  export default component;
+  const component: DefineComponent<EmailTemplateProps, {}, any>
+  export default component
 }
