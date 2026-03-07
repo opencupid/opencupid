85ebbb4823f9553d5e0f5f8f88621257be0cd210
Peter Banik <peter@froggle.org>
2026-03-07 23:53:20 +0100

    fix email compat issues

--- apps/backend/src/services/email/emailRenderer.ts
+++ apps/backend/src/services/email/emailRenderer.ts
@@ -10,10 +10,12 @@ export async function renderEmail(component: Component, payload: EmailPayload): 
   const bodyInner = await renderToString(app)
 
   const doc = `<!doctype html>
-<html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
   <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <title>${payload.subject}</title>
   <style>${emailCss}</style>
 </head>
 <body style="margin:0;padding:0;">
