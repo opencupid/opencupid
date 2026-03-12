4a1c30e967e5ae3d079e597355b58db18263c8e1
Peter Banik <peter@froggle.org>
2026-03-12 22:37:39 +0100

    fix publicName initial validation state

--- apps/frontend/src/features/shared/profileform/PublicNameInput.vue
+++ apps/frontend/src/features/shared/profileform/PublicNameInput.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { detectMobile } from '@/lib/mobile-detect'
+import { refDebounced } from '@vueuse/core'
 import { computed, onMounted, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { validatePublicName } from './publicNameValidation'
@@ -14,7 +15,11 @@ const isMobile = detectMobile()
 const inputRef = ref<HTMLInputElement | null>(null)
 
 const validationResult = computed(() => validatePublicName(model.value))
-const validated = computed(() => validationResult.value === null)
+const validated = computed<boolean | null>(() => {
+  if (model.value === '') return null
+  return validationResult.value === null
+})
+const debouncedValidated = refDebounced(validated, 1000)
 const inputFeedbackText = computed(() => {
   if (validationResult.value === 'contains_whitespace') {
     return t('profiles.forms.name_first_name_only')
@@ -49,13 +54,13 @@ onMounted(() => {
       autocomplete="off"
       ref="inputRef"
       lazy
-      :state="validated"
+      :state="debouncedValidated"
     >
     </BInput>
     <BFormInvalidFeedback
       force-show
       class="text-center"
-      :class="{ invisible: validated }"
+      :class="{ invisible: debouncedValidated !== false }"
     >
       {{ inputFeedbackText }} &nbsp;</BFormInvalidFeedback
     >
